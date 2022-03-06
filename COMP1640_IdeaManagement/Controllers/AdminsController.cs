19a1c89388603079aad5220be35d64cffc71aa78
J_Nam <NamltGCD18728@fpt.edu.vn>
2022-03-06 15:15:01 +0700

    Add Seed data function

---
+++ COMP1640_IdeaManagement/Controllers/AdminsController.cs
@@ -0,0 +1,58 @@
+﻿using COMP1640_IdeaManagement.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP1640_IdeaManagement.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminsController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Seeder()
+        {
+            var roleNames = typeof(RoleName).GetFields().ToList();
+            foreach (var role in roleNames)
+            {
+                var roleName = (string)role.GetRawConstantValue();
+                var roleInDb = await _roleManager.FindByNameAsync(roleName);
+                if (roleInDb == null)
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+
+            var adminUser = await _userManager.FindByNameAsync("admin");
+            if (adminUser == null)
+            {
+                adminUser = new IdentityUser()
+                {
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                    EmailConfirmed = true,
+                };
+
+                await _userManager.CreateAsync(adminUser, "admin123");
+                await _userManager.AddToRoleAsync(adminUser, RoleName.Administrator);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ COMP1640_IdeaManagement/Models/RoleName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP1640_IdeaManagement.Models
+{
+    public class RoleName
+    {
+        public const string Administrator = "Administrator";
+        public const string QACoordinator = "QA Coordinator";
+        public const string Staff = "Staff";
+    }
+}
