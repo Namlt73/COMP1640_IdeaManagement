b431a8203da43c40ece766c6a0829d0b4b88de60
J_Nam <NamltGCD18728@fpt.edu.vn>
2022-03-06 13:40:46 +0700

    Add project files.

---
+++ COMP1640_IdeaManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace COMP1640_IdeaManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
