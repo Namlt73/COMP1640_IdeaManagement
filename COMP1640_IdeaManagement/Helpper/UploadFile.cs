3886ac7c47c597dfff8f65301ef2f6473e1bb003
J_Nam <NamltGCD18728@fpt.edu.vn>
2022-03-09 14:53:13 +0700

    Add CRUD for Idea and UploadFile function

---
+++ COMP1640_IdeaManagement/Helpper/UploadFile.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP1640_IdeaManagement.Helpper
+{
+    public class UploadFile
+    {
+        [Required(ErrorMessage = "Please choose a file!")]
+        [DataType(DataType.Upload)]
+        [FileExtensions(Extensions ="zip, pdf, png, jpg, jpeg, gif")]
+        [Display(Name = "Upload file support idea")]
+        public IFormFile FileUpload { get; set; }
+    }
+}
