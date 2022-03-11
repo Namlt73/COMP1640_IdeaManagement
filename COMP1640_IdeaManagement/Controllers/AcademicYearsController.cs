6c9c0d15f3f167f560770904902548002f61bdfb
J_Nam <NamltGCD18728@fpt.edu.vn>
2022-03-11 15:45:35 +0700

    Create CRUD for AcademicYear

---
+++ COMP1640_IdeaManagement/Controllers/AcademicYearsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP1640_IdeaManagement.Data;
+using COMP1640_IdeaManagement.Models;
+
+namespace COMP1640_IdeaManagement.Controllers
+{
+    public class AcademicYearsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AcademicYearsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AcademicYears
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AcademicYears.ToListAsync());
+        }
+
+        // GET: AcademicYears/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var academicYear = await _context.AcademicYears
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (academicYear == null)
+            {
+                return NotFound();
+            }
+
+            return View(academicYear);
+        }
+
+        // GET: AcademicYears/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AcademicYears/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Description,StartedDate,EndedDate")] AcademicYear academicYear)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(academicYear);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(academicYear);
+        }
+
+        // GET: AcademicYears/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var academicYear = await _context.AcademicYears.FindAsync(id);
+            if (academicYear == null)
+            {
+                return NotFound();
+            }
+            return View(academicYear);
+        }
+
+        // POST: AcademicYears/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Description,StartedDate,EndedDate")] AcademicYear academicYear)
+        {
+            if (id != academicYear.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(academicYear);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AcademicYearExists(academicYear.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(academicYear);
+        }
+
+        // GET: AcademicYears/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var academicYear = await _context.AcademicYears
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (academicYear == null)
+            {
+                return NotFound();
+            }
+
+            return View(academicYear);
+        }
+
+        // POST: AcademicYears/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var academicYear = await _context.AcademicYears.FindAsync(id);
+            _context.AcademicYears.Remove(academicYear);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AcademicYearExists(int id)
+        {
+            return _context.AcademicYears.Any(e => e.Id == id);
+        }
+    }
+}
