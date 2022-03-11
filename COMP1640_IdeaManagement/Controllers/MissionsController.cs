91aa17fd3ead43f62c249e77bd8b1c3e43798935
J_Nam <NamltGCD18728@fpt.edu.vn>
2022-03-11 15:52:35 +0700

    Create CRUD for Mission

---
+++ COMP1640_IdeaManagement/Controllers/MissionsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP1640_IdeaManagement.Data;
+using COMP1640_IdeaManagement.Models;
+
+namespace COMP1640_IdeaManagement.Controllers
+{
+    public class MissionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MissionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Missions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Missions.Include(m => m.AcademicYear);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Missions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mission = await _context.Missions
+                .Include(m => m.AcademicYear)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mission == null)
+            {
+                return NotFound();
+            }
+
+            return View(mission);
+        }
+
+        // GET: Missions/Create
+        public IActionResult Create()
+        {
+            ViewData["AcademicYearId"] = new SelectList(_context.AcademicYears, "Id", "Id");
+            return View();
+        }
+
+        // POST: Missions/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,OpenDate,ClosureDate,AcademicYearId")] Mission mission)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mission);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AcademicYearId"] = new SelectList(_context.AcademicYears, "Id", "Id", mission.AcademicYearId);
+            return View(mission);
+        }
+
+        // GET: Missions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mission = await _context.Missions.FindAsync(id);
+            if (mission == null)
+            {
+                return NotFound();
+            }
+            ViewData["AcademicYearId"] = new SelectList(_context.AcademicYears, "Id", "Id", mission.AcademicYearId);
+            return View(mission);
+        }
+
+        // POST: Missions/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,OpenDate,ClosureDate,AcademicYearId")] Mission mission)
+        {
+            if (id != mission.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mission);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MissionExists(mission.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AcademicYearId"] = new SelectList(_context.AcademicYears, "Id", "Id", mission.AcademicYearId);
+            return View(mission);
+        }
+
+        // GET: Missions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mission = await _context.Missions
+                .Include(m => m.AcademicYear)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mission == null)
+            {
+                return NotFound();
+            }
+
+            return View(mission);
+        }
+
+        // POST: Missions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mission = await _context.Missions.FindAsync(id);
+            _context.Missions.Remove(mission);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MissionExists(int id)
+        {
+            return _context.Missions.Any(e => e.Id == id);
+        }
+    }
+}
